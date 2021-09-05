d09228ef8f98f681b0caabf1dde62429a82637fd
cryswerton <cryswertons@gmail.com>
2021-09-05 00:18:37 -0300

    added progress bar that follows the size of the video as it plays

--- index.php
+++ index.php
@@ -6,14 +6,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <title>PHP Video Player</title>
+    <style>
+        video{
+            margin: 0px;
+        }
+        #progress-bar{
+            margin: 0px;
+            height: 15px;
+            width: 0px;
+            background-color: red; 
+        }
+    </style>
     <script>
         document.addEventListener('DOMContentLoaded', () => {
             console.log('test')
             const vd = document.querySelector('video')
+            const progressBar = document.querySelector('#progress-bar')
 
             vd.addEventListener('click', () => {
                 vd.paused ? vd.play() : vd.pause()
             })
+
+
+            vd.onloadedmetadata = function(){
+                const videoLength = vd.duration
+                const progressBarMaxSize = 640;
+                console.log('metadata loaded')
+                setInterval(function(){ 
+                    let taxaAtual = 100 * parseFloat(vd.currentTime) / parseFloat(videoLength)
+                    let progressBarSize = taxaAtual * progressBarMaxSize / 100
+                    progressBar.style.width = `${progressBarSize}px`
+                    console.log(vd.currentTime)
+                    }, 1000);
+                }
         })
     </script>
 </head>
@@ -27,6 +52,7 @@
             <source src="videos/video.mp4" type="video/mp4">
             Your browser does not support the video tag.
         </video>
+        <div id="progress-bar"></div>
      </div>
 </body>
 </html>
